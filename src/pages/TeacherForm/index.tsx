83f98950739a7bb12bebaa7ea37488615801fb2c
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2020-08-05 23:27:14 -0300

    Implementacao layout pagina dar aulas; criacao dos componentes input, select e textarea

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,5 +1,60 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 export default function TeacherForm() {
-	return <h1>TeacherForm</h1>;
+	return (
+		<div id='page-teacher-form' className='container'>
+			<PageHeader
+				title='Que incrível que você quer dar aulas.'
+				description='O primeiro passo é preencher esse formulário de inscrição'
+			></PageHeader>
+
+			<main>
+				<fieldset>
+					<legend>Seus dados</legend>
+					<Input name='name' label='Nome completo'></Input>
+					<Input name='avatar' label='Avatar'></Input>
+					<Input name='whatsapp' label='Whatsapp'></Input>
+					<Textarea name='bio' label='Biografia' />
+				</fieldset>
+
+				<fieldset>
+					<legend>Sobre a aula</legend>
+					<Select
+						name='subject'
+						label='Matéria'
+						options={[
+							{ value: 'Artes', label: 'Artes' },
+							{ value: 'Biologia', label: 'Biologia' },
+							{ value: 'Ciências', label: 'Ciências' },
+							{ value: 'Educação física', label: 'Educação física' },
+							{ value: 'Física', label: 'Física' },
+							{ value: 'Geografia', label: 'Geografia' },
+							{ value: 'História', label: 'História' },
+							{ value: 'Matemática', label: 'Matemática' },
+							{ value: 'Português', label: 'Português' },
+							{ value: 'Química', label: 'Química' },
+						]}
+					></Select>
+					<Input name='cost' label='Custo da sua hora/aula'></Input>
+				</fieldset>
+
+				<footer>
+					<p>
+						<img src={warningIcon} alt='Aviso importante' />
+						Importante! <br />
+						Preencha todos os dados
+					</p>
+					<button type='button'>Salvar cadastro</button>
+				</footer>
+			</main>
+		</div>
+	);
 }
